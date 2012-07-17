62893c4329a6827a61e828cef6b9776333084284
Benjie Chen <benjie@ginkgobioworks.com>
2012-07-17 11:32:32 -0400

    make URL to notebook server configrable via django settings

--- djnbmgr/admin.py
+++ djnbmgr/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from djnbmgr.models import *
-
+from django.conf import settings
 
 class NotebookAdmin(admin.ModelAdmin):
   def _name(self,obj):
-    return '<a href="http://localhost:8888/'+str(obj.id)+'">'+str(obj.name)+'</a>'
+    nbsrv = 'http://localhost:8888'
+    if settings.IPYTHON_NOTEBOOK_SERVER:
+      nbsrv = settings.IPYTHON_NOTEBOOK_SERVER
+    return '<a href="'+str(nbsrv)+'/'+str(obj.id)+'" target="_blank">'+str(obj.name)+'</a>'
   _name.short_description = 'Name'
   _name.allow_tags = True
   
