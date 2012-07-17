00b20e6a704d7dfe1917e95de0154c192fef11a6
Benjie Chen <benjie@ginkgobioworks.com>
2012-07-17 00:27:51 -0400

    better history management, and a skeleton admin setup (although URL does not work yet)

--- djnbmgr/admin.py
+++ djnbmgr/admin.py
@@ -4,7 +4,7 @@ from djnbmgr.models import *
 
 class NotebookAdmin(admin.ModelAdmin):
   def _name(self,obj):
-    return '<a href="http://hydrogen.ginkgobioworks.com:8888/'+str(obj.id)+'">'+str(obj.name)+'</a>'
+    return '<a href="http://localhost:8888/'+str(obj.id)+'">'+str(obj.name)+'</a>'
   _name.short_description = 'Name'
   _name.allow_tags = True
   
